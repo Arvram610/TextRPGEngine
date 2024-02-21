a304d420b1c61807314e6593e198f1e05ca5a11b
Oscar <oscag274@student.liu.se>
2024-02-21 14:59:20 +0100

    Adds Consumables and Equippables

---
+++ src/se/liu/arvra591/models/items/Consumables.java
@@ -0,0 +1,16 @@
+package se.liu.arvra591.models.items;
+
+public class Consumables extends Item
+{
+    private int stacksize; //how many of the item can be stacked in one slot
+    private int amount; //how many of the item is in the slot ex 5 healthpotions
+    Consumables(final String name, final String description, final int weight, final int stacksize, final int amount) {
+	super(name, description, weight);
+	this.stacksize = stacksize;
+	this.amount = amount;
+    }
+
+    private void use(){
+	//uses and consumes the item
+    }
+}
---
+++ src/se/liu/arvra591/models/items/Equippables.java
@@ -0,0 +1,9 @@
+package se.liu.arvra591.models.items;
+
+public class Equippables extends Item
+{
+    private int stats; //might be changed later, ex armor, damage, etc
+    Equippables(final String name, final String description, final int weight) {
+	super(name, description, weight);
+    }
+}
