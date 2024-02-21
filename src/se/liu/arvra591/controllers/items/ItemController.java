eab47995b728a365dbf9a3520234e6841a43606c
arvid <arvid@ramsberg.nu>
2024-02-21 15:26:04 +0100

    Moves javadoc in itemcontroller

--- src/se/liu/arvra591/controllers/items/ItemController.java
+++ src/se/liu/arvra591/controllers/items/ItemController.java
@@ -1,11 +1,13 @@
 package se.liu.arvra591.controllers.items;
-/**
- * Class to control items
- */
+
 import se.liu.arvra591.controllers.AbstractObjectController;
 import se.liu.arvra591.models.items.Item;
 import se.liu.arvra591.view.items.ItemView;
 
+
+/**
+ * Class to control items
+ */
 public class ItemController extends AbstractObjectController
 {
     public ItemController(final Item model, final ItemView view) {
