f76f2e7a541ef0b98fe79b074ca4c169863c3794
arvid <arvid@ramsberg.nu>
2024-02-21 15:31:10 +0100

    Writes simple printmodel func for view

--- src/se/liu/arvra591/view/AbstractObjectView.java
+++ src/se/liu/arvra591/view/AbstractObjectView.java
@@ -1,8 +1,16 @@
 package se.liu.arvra591.view;
+
+import se.liu.arvra591.models.AbstractObject;
+
 /**
  * Superclass for all viewer-classes
  */
 public abstract class AbstractObjectView
 {
 
+    public void printModel(final AbstractObject model) {
+        System.out.println("Name: " + model.getName());
+        System.out.println("Description: " + model.getDescription());
+        System.out.println("Type: " + model.getClass().getSimpleName());
+    }
 }
