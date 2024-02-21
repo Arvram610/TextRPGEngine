403891c2cccbdf139947eaa37d09612867eec113
arvid <arvid@ramsberg.nu>
2024-02-21 15:25:04 +0100

    Adds creaturestats

---
+++ src/se/liu/arvra591/models/creatures/CreatureStats.java
@@ -0,0 +1,47 @@
+package se.liu.arvra591.models.creatures;
+
+public class CreatureStats
+{
+    private int maxHealth;
+    private int attack;
+    private int defense;
+    private int carryWeight;
+    private int maxEnergy;
+    private int energyRegenRate;
+
+
+    public CreatureStats(final int maxHealth, final int attack, final int defense, final int carryWeight, final int maxEnergy,
+                         final int energyRegenRate)
+    {
+        this.maxHealth = maxHealth;
+        this.attack = attack;
+        this.defense = defense;
+        this.carryWeight = carryWeight;
+        this.maxEnergy = maxEnergy;
+        this.energyRegenRate = energyRegenRate;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getCarryWeight() {
+        return carryWeight;
+    }
+
+    public int getMaxEnergy() {
+        return maxEnergy;
+    }
+
+    public int getEnergyRegenRate() {
+        return energyRegenRate;
+    }
+}
