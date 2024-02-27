5241d4d886eb101c227e2ebfa757873b7f9354da
Oscar <agren.oscar@gmail.com>
2024-02-27 12:08:04 +0100

    Progress on inventory

--- src/se/liu/arvra591/objects/creatures/itemContainers/PlayerInventory.java
+++ src/se/liu/arvra591/objects/creatures/itemContainers/PlayerInventory.java
@@ -1,13 +1,18 @@
 package se.liu.arvra591.objects.creatures.itemContainers;
 
+import se.liu.arvra591.objects.creatures.PlayerStats;
 import se.liu.arvra591.objects.items.Item;
 
 import java.util.List;
 
 public class PlayerInventory extends AbstractInventory
 {
+    protected PlayerStats playerStats;
 
-    public PlayerInventory(List<Item> itemList){
+    public PlayerInventory(List<Item> itemList, PlayerStats stats){
 	super(itemList);
+	this.playerStats = stats;
     }
+
+
 }
