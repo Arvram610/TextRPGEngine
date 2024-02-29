c14d98caa3fc1f0c8e0cd48b19da868d73b339b2
Arvid Rämsberg <arvra591@student.liu.se>
2024-02-29 10:37:17 +0100

    creates jsonParser

---
+++ src/se/liu/arvra591/jsonParser/JsonParser.java
@@ -0,0 +1,31 @@
+package se.liu.arvra591.jsonParser;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonParser
+{
+    protected Gson gson;
+
+    public JsonParser(){
+	gson = new Gson();
+    }
+
+    public JsonObject parseFile(String path) throws IOException {
+	String json;
+
+	try (BufferedReader br = new BufferedReader(
+		new FileReader(path))){
+	    json = br.readLine();
+	} catch (IOException e) {
+	    throw e;
+	}
+
+	return gson.fromJson(json, JsonObject.class);
+    }
+}
