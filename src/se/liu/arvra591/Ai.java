81063331ff32a1498d3820c49a0256183beff1f7
Oscar <agren.oscar@gmail.com>
2024-03-01 17:07:18 +0100

    adds empty methods to AI

--- src/se/liu/arvra591/Ai.java
+++ src/se/liu/arvra591/Ai.java
@@ -1,6 +1,26 @@
 package se.liu.arvra591;
 
+import se.liu.arvra591.objects.creatures.Npc;
+
 public class Ai
 {
+    private Npc npc;
+    public Ai(Npc npc) {
+	this.npc = npc;
+    }
 
+    public void attack(){
+
+    }
+
+    public void useItem(){
+
+    }
+
+    public void rest(){
+
+    }
+    public void performAction(String input)
+    {
+    }
 }
