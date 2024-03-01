62c279965d01d68eedb56edef19d48cdb5fb7509
Arvid Rämsberg <arvra591@student.liu.se>
2024-03-01 15:21:55 +0100

    makes listhelper better

--- src/se/liu/arvra591/objects/containers/ObjectContainer.java
+++ src/se/liu/arvra591/objects/containers/ObjectContainer.java
@@ -42,10 +42,10 @@ public class ObjectContainer <T extends AbstractObject>
      * @return Returns the Object removed from the container
      */
     public T removeObject(String name){
-	AbstractObject object = ListHelper.findObjectInList(objects, name);
+	T object = ListHelper.findObjectInList(objects, name);
 	if (object != null) {
 	    objects.remove(object);
-	    return (T) object;
+	    return object;
 	}
 	return null;
     }
