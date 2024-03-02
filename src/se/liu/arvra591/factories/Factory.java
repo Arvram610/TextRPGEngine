61ebc4015299d391123fa7e16f069c95b260c554
Arvid Rämsberg <arvra591@student.liu.se>
2024-03-02 13:52:57 +0100

    writes javadoc for Factory.java

--- src/se/liu/arvra591/factories/Factory.java
+++ src/se/liu/arvra591/factories/Factory.java
@@ -3,16 +3,30 @@ package se.liu.arvra591.factories;
 import com.google.gson.Gson;
 import se.liu.arvra591.objects.AbstractObject;
 
+/**
+ * A class that generates copies of a given item
+ *
+ * @param <T> The type of object the factory generates
+ */
 public class Factory<T extends AbstractObject>
 {
     protected T targetObject;
     protected Gson gson;
 
+    /**
+     * @param targetObject The targetobject that the factory clones
+     */
     public Factory(T targetObject){
 	this.targetObject = targetObject;
 	gson = new Gson();
     }
 
+    /**
+     * The method used to clone the object
+     * Says it does unchecked cast, but all casts are garanteed to be of type T
+     *
+     * @return Returns a copy of the object
+     */
     @SuppressWarnings("unchecked")
     public T gen() {
 	return (T) gson.fromJson(gson.toJson(targetObject), targetObject.getClass());
