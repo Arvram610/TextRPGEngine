0156f55e6edb9c043f6fa1825eb49b3bbb592ff5
Oscar <oscag274@student.liu.se>
2024-03-20 16:22:55 +0100

    Adds javadocs to listeners

--- src/se/liu/arvra591/game/listeners/EngageEventHandler.java
+++ src/se/liu/arvra591/game/listeners/EngageEventHandler.java
@@ -7,18 +7,27 @@ public class EngageEventHandler
 {
     private EngageListener listener = null;
 
+    /**
+     * @param listener The listener that is notified when the player has engaged or disengaged
+     */
     public void setListener(EngageListener listener)
     {
 	this.listener = listener;
     }
 
+    /**
+     * @param input The npc to engage
+     */
     public void engage(String input)
     {
 	listener.engage(input);
     }
 
-    public void disengage(String input)
+    /**
+     * @param ignored Will be empty string
+     */
+    public void disengage(String ignored)
     {
-	listener.disengage(input);
+	listener.disengage(ignored);
     }
 }
