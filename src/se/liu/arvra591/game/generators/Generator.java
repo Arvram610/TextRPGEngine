65309ed1d1ecf294b3822a4b6148483eb2bb317b
Oscar <oscag274@student.liu.se>
2024-03-20 17:05:01 +0100

    removes unnecessary import in Generator

--- src/se/liu/arvra591/game/generators/Generator.java
+++ src/se/liu/arvra591/game/generators/Generator.java
@@ -13,7 +13,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 /**
  * An abstract class used to generate objects from a json file
